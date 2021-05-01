1db99bb2ba8473543dbe7ab63c6fdd26095371e5
Erawan Mukti <airexhck@gmail.com>
2021-05-02 06:00:10 +0700

    hello world dan html_php

---
+++ html_php.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Belajar HTML - PHP</title>
+    </head>
+</html>
+<body>
+    Ini adalah script HTML
+    <br/>
+    <?php echo "dan ini adalah script PHP"; ?>
+</body>
